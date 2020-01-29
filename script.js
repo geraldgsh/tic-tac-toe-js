cf08875df3fe5804167faaecab22c5848b5c2fc5
Izaias Neto <izaiasneto4@gmail.com>
2020-01-29 15:01:34 -0200

    Added the function the change the turn

--- script.js
+++ script.js
@@ -10,32 +10,34 @@ const gameEngine = (() => {
   console.log("game engine started");
 
   const start = () => {
-    var sq = document.getElementById('squares');
+    var sq = document.getElementById("squares");
     sq.style.display = "grid";
-  }  
-  const startBtn = document.querySelector('.startButton');
-  startBtn.addEventListener('click', start);
-  
+  };
 
-  const newPlayer = () => {}
-  
+  const startBtn = document.querySelector(".startButton");
+  startBtn.addEventListener("click", start);
+
+  const gameStatus = document.querySelector(".game-status");
+  const setStatus = status => {
+    gameStatus.innerHTML = "";
+    gameStatus.innerHTML += status.toString();
+  };
+
   // global variable declaration
   const player1Name = document.querySelector("#playerOneName").value;
   const player2Name = document.querySelector("#playerTwoName").value;
   const checkNames = (name1, name2) => {
     if (name1 === "" || name2 === "") {
-      return alert("names cant be blank");
+      setStatus("Names can't be blank!");
     }
   };
   checkNames(player1Name, player2Name);
-  
 
   // declaration of the players
   const player1 = playerGenerator(player1Name, "X", 0);
   const player2 = playerGenerator(player2Name, "O", 0);
   console.warn(player1, player2);
 
-  const gameStatus = document.querySelector(".game-status");
   let numPlays = 0;
   let currPlayer = player1;
 
@@ -43,16 +45,18 @@ const gameEngine = (() => {
   let cells = document.querySelectorAll(".cell");
 
   cells.forEach(cell => {
-    cell.addEventListener("click", function () {
-      checkPlay(cells, this.dataset.index)
+    cell.addEventListener("click", function() {
+      checkPlay(cells, this.dataset.index);
     });
   });
 
-  const changePlayers = currPlayer => {
+  const changePlayers = () => {
     if (currPlayer === player1) {
-      return currPlayer = player2;
+      currPlayer = player2;
+      setStatus(`It's ${currPlayer.name}'s turn!`);
     } else {
-      return currPlayer = player1;
+      currPlayer = player1;
+      setStatus(`It's ${currPlayer.name}'s turn!`);
     }
   };
 
@@ -60,20 +64,7 @@ const gameEngine = (() => {
     if (board[cellNum] === "") {
       board[cellNum] = currPlayer.mark;
       cells[cellNum].innerHTML += currPlayer.mark;
-      changePlayers(currPlayer);
+      changePlayers();
     }
   };
-
-  const setStatus = status => {
-    gameStatus.innerHTML = "";
-    gameStatus.innerHTML += status.toString();
-  };
-
-  // const gameplay = () => {
-  //   while (gameComplete()) {}
-  // };
-
-  // const restart = () => {
-
-  // }
 })();
