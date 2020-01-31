ee8f941da99ccccd9706fe605996e2789777a64c
Izaias Neto <izaiasneto4@gmail.com>
2020-01-31 18:30:04 -0200

    Fixed majority of linter errors

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const playBoard = (() => {
-  const gridBoard = ["", "", "", "", "", "", "", "", ""];
+  const gridBoard = ['', '', '', '', '', '', '', '', ''];
   const winningCombination = [
     [0, 1, 2],
     [3, 4, 5],
@@ -8,44 +8,41 @@ const playBoard = (() => {
     [2, 4, 6],
     [0, 3, 6],
     [1, 4, 7],
-    [2, 5, 8]
+    [2, 5, 8],
   ];
 
   return {
     gridBoard,
-    winningCombination
+    winningCombination,
   };
 })();
 
-const playerGenerator = (name, mark, wins, playArr) => {
-  return {
-    name,
-    mark,
-    wins,
-    playArr
-  };
-};
+const playerGenerator = (name, mark, wins, playArr) => ({
+  name,
+  mark,
+  wins,
+  playArr,
+});
 
 const gameEngine = (() => {
-  console.log("game engine started");
   // declaration of the players
-  let player1 = playerGenerator("", "X", 0, []);
-  let player2 = playerGenerator("", "O", 0, []);
+  let player1 = playerGenerator('', 'X', 0, []);
+  let player2 = playerGenerator('', 'O', 0, []);
 
-  const gameStatus = document.querySelector(".game-status");
+  const gameStatus = document.querySelector('.game-status');
   const setStatus = status => {
-    gameStatus.innerHTML = "";    
+    gameStatus.innerHTML = '';
     gameStatus.innerHTML += status.toString();
   };
-  setStatus("");
+  setStatus('');
 
-  const winner = document.querySelector(".winner-status");
+  const winner = document.querySelector('.winner-status');
   const setWinner = status => {
-    winner.innerHTML = "";
+    winner.innerHTML = '';
     winner.innerHTML += status.toString();
   };
 
-  const scoreboard= () => {
+  const scoreboard = () => {
     const score = document.querySelector('.score-board');
     score.innerHTML = `
     <p>Player 'X': ${player1.name} ${player1.wins} </p>
@@ -54,61 +51,59 @@ const gameEngine = (() => {
   };
 
   const start = () => {
-    var sq = document.getElementById("squares");
+    const sq = document.getElementById('squares');
     newPlayer(player1);
     newPlayer(player2);
     if (checkNames(player1.name, player2.name)) {
-      sq.style.display = "grid";
+      sq.style.display = 'grid';
     }
     scoreboard();
   };
 
-  const startBtn = document.querySelector(".startButton");
-  startBtn.addEventListener("click", start);
+  const startBtn = document.querySelector('.startButton');
+  startBtn.addEventListener('click', start);
 
   const newPlayer = player => {
     if (player === player1) {
-      let player1Name = document.querySelector("#playerOneName").value;
+      const player1Name = document.querySelector('#playerOneName').value;
       player1.name = player1Name;
     } else {
-      let player2Name = document.querySelector("#playerTwoName").value;
+      const player2Name = document.querySelector('#playerTwoName').value;
       player2.name = player2Name;
     }
   };
 
   const checkNames = (name1, name2) => {
-    if (name1 === "" || name2 === "") {
+    if (name1 === '' || name2 === '') {
       setStatus("Names can't be blank!");
       return false;
     }
     return true;
   };
 
-  console.warn(player1, player2);
-
   let numPlays = 0;
   let currPlayer = player1;
 
-  let cells = document.querySelectorAll(".cell");
+  const cells = document.querySelectorAll('.cell');
   cells.forEach(cell => {
-    cell.addEventListener("click", function () {
+    cell.addEventListener('click', function setEvtListener() {
       checkPlay(cells, this.dataset.index);
     });
   });
 
   const clearBoard = () => {
     numPlays = 0;
-    playBoard.gridBoard = ["", "", "", "", "", "", "", "", ""];
+    playBoard.gridBoard = ['', '', '', '', '', '', '', '', ''];
     player1 = playerGenerator(player1.name, player1.mark, player1.wins, []);
     player2 = playerGenerator(player2.name, player2.mark, player2.wins, []);
-    const pieces = document.querySelectorAll('.cell')
+    const pieces = document.querySelectorAll('.cell');
     pieces.forEach(cell => {
-      cell.innerHTML = "";
-    })
+      cell.innerHTML = '';
+    });
     scoreboard();
     currPlayer = player1;
-    changePlayers();    
-  }
+    changePlayers();
+  };
 
   const changePlayers = () => {
     if (currPlayer === player1) {
@@ -117,43 +112,40 @@ const gameEngine = (() => {
       currPlayer = player1;
     }
     setStatus(`It's ${currPlayer.name}'s turn!`);
-   };
+  };
 
   const checkWinner = arr => {
-    const result = playBoard.winningCombination.some(evt =>
-      evt.every(e => arr.includes(e))
-    );
+    const result = playBoard.winningCombination.some(evt => evt.every(e => arr.includes(e)));
     return result;
   };
 
   const checkPlay = (cells, cellNum) => {
-    if (playBoard.gridBoard[cellNum] === "") {
+    if (playBoard.gridBoard[cellNum] === '') {
       playBoard.gridBoard[cellNum] = currPlayer.mark;
       cells[cellNum].innerHTML += currPlayer.mark;
-      currPlayer.playArr.push(parseInt(cellNum));
+      currPlayer.playArr.push(Number(cellNum));
 
       if (checkWinner(currPlayer.playArr)) {
-        
-        const modal = document.getElementById("myModal");
-        modal.style.display = "block";
+        const modal = document.getElementById('myModal');
+        modal.style.display = 'block';
         endGame();
-        
-        const span = document.getElementsByClassName("close")[0]; 
-        span.onclick = function () {
-          modal.style.display = "none";
+
+        const span = document.getElementsByClassName('close')[0];
+        span.onclick = function setSpan() {
+          modal.style.display = 'none';
           clearBoard();
           newRound();
-        }           
+        };
       } else {
         changePlayers();
-        numPlays++;
+        numPlays += 1;
         // detecting a draw
         if (numPlays > 8) {
           setStatus("It's a tie!");
         }
       }
     } else {
-      setStatus("Stop clicking!");
+      setStatus('Stop clicking!');
     }
   };
 
@@ -163,7 +155,7 @@ const gameEngine = (() => {
   };
 
   const newRound = () => {
-    playBoard.gridBoard = ["", "", "", "", "", "", "", "", ""];
+    playBoard.gridBoard = ['', '', '', '', '', '', '', '', ''];
     if (player1 === currPlayer) {
       player1 = playerGenerator(player1.name, player1.mark, player1.wins += 1, []);
       player2 = playerGenerator(player2.name, player2.mark, player2.wins, []);
@@ -173,7 +165,9 @@ const gameEngine = (() => {
     }
     scoreboard();
     changePlayers();
-  }
+  };
 
   document.querySelector('.game-restart').addEventListener('click', clearBoard);
 })();
+
+gameEngine();
